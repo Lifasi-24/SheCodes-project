32ac0ac2f0151b08ce3ae682f2fbe0c8a429894a
Lifasi-24 <lifasiqueenm@gmail.com>
2025-09-09 22:54:37 +0200

    Added basic search-engine, no API

---
+++ script.js
@@ -0,0 +1,9 @@
+function displayCity(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInputElement.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", displayCity);
