f89c7bd2ee09fdccd23cb1e593847eaa975ec3dd
Lifasi-24 <lifasiqueenm@gmail.com>
2025-09-12 00:32:16 +0200

    injected icon

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
@@ -13,6 +14,7 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   timeElement.innerHTML = formatDate(date);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temperature-icon"/> `;
 }
 
 function formatDate(date) {
